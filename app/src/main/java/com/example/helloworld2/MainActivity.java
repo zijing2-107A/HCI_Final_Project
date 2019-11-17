9d9b923ac68261b36a8226f90e11f1ef810a936e
xdbwk <xindubawukong@outlook.com>
2019-11-17 21:44:21 +0800

    Can change screen orientation by onFling

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld2;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
@@ -9,11 +10,17 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.GestureDetector;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        gestureDetector = new GestureDetector(this, this);
     }
 
     @Override
@@ -54,5 +63,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //当手指按下的时候触发下面的方法
+    @Override
+    public boolean onDown(MotionEvent e) {
+        System.out.println("onDown");
+        return false;
+    }
+
+    //当用户手指在屏幕上按下,而且还未移动和松开的时候触发这个方法
+    @Override
+    public void onShowPress(MotionEvent e) {
+        System.out.println("onShowPress");
+    }
+
+    //当手指在屏幕上轻轻点击的时候触发下面的方法
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        System.out.println("onSingleTapUp");
+        return false;
+    }
+
+    //当手指在屏幕上滚动的时候触发这个方法
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        System.out.println("onScroll");
+        return false;
+    }
+
+    //当用户手指在屏幕上长按的时候触发下面的方法
+    @Override
+    public void onLongPress(MotionEvent e) {
+        System.out.println("onLongPress");
+    }
+
+    //当用户的手指在触摸屏上拖过的时候触发下面的方法,velocityX代表横向上的速度,velocityY代表纵向上的速度
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        System.out.println("onFling");
+        int angle = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+        int flag = 0;
+        if (Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
+            if (e1.getX() > e2.getX()) flag = 3;
+            else flag = 1;
+        }
+        else {
+            if (e1.getY() < e2.getY()) flag = 2;
+            else flag = 0;
+        }
+        int orientation = (flag + angle) % 4;
+        System.out.println("angle: " + angle + "   flag:" + flag + "    orientation:" + orientation);
+        if (orientation == 0) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        else if (orientation == 1) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        else if (orientation == 2) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+        }
+        else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
     }
 }
