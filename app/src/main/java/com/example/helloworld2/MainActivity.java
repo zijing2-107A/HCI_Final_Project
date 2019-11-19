950961d85ed5daf79ce9675ec8aabebbb1209a55
zxh <zhaoxinhao2012@126.com>
2019-11-19 10:57:36 +0800

    add landscape control

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.helloworld2;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,17 +14,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.provider.Settings;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.GestureDetector;
-import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class MainActivity extends AppCompatActivity{
 
-    GestureDetector gestureDetector;
-    int finger_count = 0;
+    SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +36,10 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
-                Settings.System.putInt(getContentResolver(),Settings.System. ACCELEROMETER_ROTATION,0);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-
-        gestureDetector = new GestureDetector(this, this);
 
         startService(new Intent(getBaseContext(), MyService.class));
     }
@@ -77,69 +72,4 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         return super.onOptionsItemSelected(item);
     }
 
-    //当手指按下的时候触发下面的方法
-    @Override
-    public boolean onDown(MotionEvent e) {
-        System.out.println("onDown");
-        return false;
-    }
-
-    //当用户手指在屏幕上按下,而且还未移动和松开的时候触发这个方法
-    @Override
-    public void onShowPress(MotionEvent e) {
-        System.out.println("onShowPress");
-    }
-
-    //当手指在屏幕上轻轻点击的时候触发下面的方法
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        System.out.println("onSingleTapUp");
-        return false;
-    }
-
-    //当手指在屏幕上滚动的时候触发这个方法
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        int finger_count_now = e2.getPointerCount();
-        if(finger_count_now > finger_count) finger_count = finger_count_now;
-        System.out.println("scroll count: " + finger_count_now);
-        System.out.println("onScroll");
-        return false;
-    }
-
-    //当用户手指在屏幕上长按的时候触发下面的方法
-    @Override
-    public void onLongPress(MotionEvent e) {
-        System.out.println("onLongPress");
-    }
-
-    //当用户的手指在触摸屏上拖过的时候触发下面的方法,velocityX代表横向上的速度,velocityY代表纵向上的速度
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        System.out.println("onFling");
-        if(finger_count != 2){
-            finger_count = 1;
-            return false;
-        }
-        finger_count = 1;
-        int angle = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
-        int flag = 0;
-        if (Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
-            if (e1.getX() > e2.getX()) flag = 3;
-            else flag = 1;
-        }
-        else {
-            if (e1.getY() < e2.getY()) flag = 2;
-            else flag = 0;
-        }
-        int orientation = (flag + angle) % 4;
-        System.out.println("angle: " + angle + "   flag:" + flag + "    orientation:" + orientation);
-        Settings.System.putInt(getContentResolver(),Settings.System. USER_ROTATION, orientation);
-        return false;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
-    }
 }
--- app/src/main/java/com/example/helloworld2/MyService.java
+++ app/src/main/java/com/example/helloworld2/MyService.java
@@ -2,10 +2,30 @@ package com.example.helloworld2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.util.Log;
+import android.util.TimeUtils;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyService extends Service {
+    SensorManager sensorManager;
+
+    // 策略：采用过去n次重力值平均值作为稳定值，随后如果重力值偏离稳定值超过k，说明需要进行操作
+    final int historyNum = 10;
+    final int deltaBoundary = 5;
+    final int controlNum = 3; // 使用3次重力变化控制屏幕方向
+    final int millisBoundary = 500; // 两次大幅度重力变化的时间差
+    List<Float> historyGravity = new ArrayList<Float>();
+    List<Float> landscapeControl = new ArrayList<Float>();
+    long lastTime = 0; // 上次发生大幅度重力变化的时间
+
     public MyService() {
     }
 
@@ -19,6 +39,10 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Toast.makeText(getBaseContext(),"MyService onCreate", Toast.LENGTH_LONG).show();
+        // 重力感应
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -26,4 +50,55 @@ public class MyService extends Service {
         super.onDestroy();
         Toast.makeText(getBaseContext(),"MyService onDestroy", Toast.LENGTH_LONG).show();
     }
+
+    final SensorEventListener accelerometerListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+//                Log.i("sensor","onSensorChanged");
+                //图解中已经解释三个值的含义
+                float X_lateral = event.values[0];
+                float Y_longitudinal = event.values[1];
+                float Z_vertical = event.values[2];
+
+                float historyMean = calculateAverage(historyGravity);
+                historyGravity.add(X_lateral);
+                if(historyGravity.size() > historyNum){
+                    historyGravity.remove(0);
+                    if(X_lateral < historyMean-deltaBoundary || X_lateral > historyMean+deltaBoundary){
+                        long currTime = System.currentTimeMillis();
+                        if(currTime - lastTime > millisBoundary) landscapeControl.clear();
+                        lastTime = currTime;
+
+                        float delta = X_lateral-historyMean;
+                        Log.i("sensor","\n heading "+delta);
+                        landscapeControl.add(delta);
+
+                        if(landscapeControl.size() == controlNum){
+                            if(landscapeControl.get(0) < 0 && landscapeControl.get(1) > 0 && landscapeControl.get(2) < 0) ;
+                            else if(landscapeControl.get(0) > 0 && landscapeControl.get(1) < 0 && landscapeControl.get(2) > 0);
+                            landscapeControl.clear();
+                        }
+                    }
+                }
+                else Log.i("sensor", "no enough history");
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//            Log.i("sensor", "onAccuracyChanged");
+        }
+
+        private float calculateAverage(List<Float> array){
+            float sum = 0;
+            for(Float num : array){
+                sum += num;
+            }
+            if(array.isEmpty()) return 0;
+            else return sum/array.size();
+        }
+    };
 }
