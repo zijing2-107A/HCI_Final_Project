5480dc6257c09cfe86f8c46be29f7b47ccfc2d6c
xdbwk <xindubawukong@outlook.com>
2019-12-16 20:22:37 +0800

    好像可以用了卧槽

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - https://www.jianshu.com/p/a1df3476c82f
 - https://zhuanlan.zhihu.com/p/46189118?utm_source=wechat_session&utm_medium=social&utm_oi=603232655068762112
 - https://www.runoob.com/android/android-tutorial.html
+- https://www.cnblogs.com/android-deli/p/10155183.html
+- https://www.cnblogs.com/Ricardoldc/p/10961646.html
--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -1,137 +1,295 @@
 package com.example.helloworld2;
-
-import android.content.Context;
-import android.content.pm.ActivityInfo;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.media.FaceDetector;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.GestureDetector;
-import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    GestureDetector gestureDetector;
-    int finger_count = 0;
+public class MainActivity extends Activity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    /**
+     * 操作的是SurfaceHolder，所以定义全局变量
+     */
+    private SurfaceHolder surfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        Settings.System.putInt(getContentResolver(),Settings.System. ACCELEROMETER_ROTATION,0);
+        setContentView(R.layout.content_main);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        // 获取在布局文件中定义的SurfaceView
+        SurfaceView surfaceView = findViewById(R.id.surface_view);
+
+        // 不能直接操作SurfaceView，需要通过SurfaceView拿到SurfaceHolder
+        surfaceHolder = surfaceView.getHolder();
+
+//        // 使用SurfaceHolder设置屏幕高亮，注意：所有的View都可以设置 设置屏幕高亮
+//        surfaceHolder.setKeepScreenOn(true);
+//
+//        // 使用SurfaceHolder设置把画面或缓存 直接显示出来
+//        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        /**
+         * 由于要观察SurfaceView生命周期，所以需要设置监听
+         * 此监听不一样，是addCallback
+         *
+         */
+        surfaceHolder.addCallback(callback);
+    }
+
+    /**
+     * 定义相机对象
+     */
+    private Camera camera;
+
+    /**
+     * 定义SurfaceView监听回调
+     */
+    private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
+
+        /**
+         * SurfaceView被创建了
+         * @param holder
+         */
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            /**
+             * 只有在SurfaceView生命周期方法-->SurfaceView被创建后在打开相机
+             * 以前我在 onResume 之前去打开相机，结果报错了，所以只有在这里打开相机，才是安全🔐
+             */
+            // Camera要选择hardware.Camera，因为Camera属于硬件hardware
+            // Camera.open(1); // 这了传入的值，可以指定为：前置摄像头/后置摄像头
+            camera = Camera.open(1);
+
+            /**
+             * 设置Camera与SurfaceHolder关联，Camera的数据让SurfaceView显示
+             */
+            try {
+                camera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "相机设置预览失败");
             }
-        });
 
-        gestureDetector = new GestureDetector(this, this);
-    }
+            camera.setFaceDetectionListener(new FaceDetectorListener());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            /**
+             * 开始显示
+             */
+            camera.startPreview();
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        /**
+         * SurfaceView发生改变了
+         * @param holder
+         * @param format
+         * @param width
+         * @param height
+         */
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            // 以后做具体业务功能的时候，才会用到改变的值来处理
+            // ......
+            System.out.println("surfaceChanged");
+            if (surfaceHolder.getSurface() == null) {
+                // preview surface does not exist
+                Log.e(TAG, "mHolder.getSurface() == null");
+                return;
+            }
 
-    //当手指按下的时候触发下面的方法
-    @Override
-    public boolean onDown(MotionEvent e) {
-        System.out.println("onDown");
-        return false;
-    }
+            try {
+                camera.stopPreview();
 
-    //当用户手指在屏幕上按下,而且还未移动和松开的时候触发这个方法
-    @Override
-    public void onShowPress(MotionEvent e) {
-        System.out.println("onShowPress");
-    }
+            } catch (Exception e) {
+                // ignore: tried to stop a non-existent preview
+                Log.e(TAG, "Error stopping camera preview: " + e.getMessage());
+            }
 
-    //当手指在屏幕上轻轻点击的时候触发下面的方法
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        System.out.println("onSingleTapUp");
-        return false;
-    }
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
 
-    //当手指在屏幕上滚动的时候触发这个方法
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        int finger_count_now = e2.getPointerCount();
-        if(finger_count_now > finger_count) finger_count = finger_count_now;
-        System.out.println("scroll count: " + finger_count_now);
-        System.out.println("onScroll");
-        return false;
-    }
+                startFaceDetection(); // re-start face detection feature
 
-    //当用户手指在屏幕上长按的时候触发下面的方法
-    @Override
-    public void onLongPress(MotionEvent e) {
-        System.out.println("onLongPress");
-    }
-
-    //当用户的手指在触摸屏上拖过的时候触发下面的方法,velocityX代表横向上的速度,velocityY代表纵向上的速度
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        System.out.println("count: " + finger_count);
-        System.out.println("onFling");
-        if(finger_count != 2){
-            finger_count = 1;
-            return false;
+            } catch (Exception e) {
+                // ignore: tried to stop a non-existent preview
+                Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            }
         }
-        finger_count = 1;
-        int angle = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
-        int flag = 0;
-        if (Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
-            if (e1.getX() > e2.getX()) flag = 3;
-            else flag = 1;
+
+        /**
+         * SurfaceView被销毁了
+         * @param holder
+         */
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            /**
+             * SurfaceView被销毁后，一定要释放硬件资源，Camera是硬件
+             */
+            camera.stopPreview(); // (一定要有，不然只release也可能出问题)
+            camera.release();
+            camera = null;
+            System.gc();
         }
-        else {
-            if (e1.getY() < e2.getY()) flag = 2;
-            else flag = 0;
+    };
+
+    /**
+     * 拍照
+     */
+    public void takepicture(View view) {
+        if (null != camera) {
+            /**
+             * 参数一：相机的快门，咔嚓，这里用不到
+             * 参数二：相机原始的数据
+             * 参数三：相机处理后的数据
+             */
+            camera.takePicture(null, null, pictureCallback);
         }
-        int orientation = (flag + angle) % 4;
-        System.out.println("angle: " + angle + "   flag:" + flag + "    orientation:" + orientation);
-        Settings.System.putInt(getContentResolver(),Settings.System. USER_ROTATION, orientation);
-        return false;
     }
 
+    /**
+     * 相机处理后的数据回调
+     */
+    private Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        /**
+         * 拍照完成，可以拿到数据了
+         * @param data 数据
+         * @param camera
+         */
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+//            // 写在指定目录 sdcard 中去
+//            File file = new File("/storage/Pictures/Screenshots", "pictureCallback.jpg");
+//            try {
+//                // 字节文件输出流，把byte[]数据写入到文件里面去
+//                OutputStream os = new FileOutputStream(file);
+//                os.write(data);
+//
+//                // 关闭字节文件输出流
+//                os.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                Log.e(TAG, "保存失败");
+//                Toast.makeText(MainActivity.this, "照片保存失败", Toast.LENGTH_SHORT).show();
+//            }
+            Bitmap bitmap0 = BitmapFactory.decodeByteArray(data,0,data.length);
+            bitmap0 = bitmap0.copy(Bitmap.Config.RGB_565, true);
+            System.out.println("onPictureTaken");
+            System.out.println("width: " + bitmap0.getWidth());
+            System.out.println("height: " + bitmap0.getHeight());
+            for (int i = 0; i < 4; i++) {
+                Bitmap bitmap = rotateBitmap(bitmap0, 90 * i);
+                FaceDetector detector = new FaceDetector(bitmap.getWidth(),bitmap.getHeight(), 1);
+                FaceDetector.Face[] faces = new FaceDetector.Face[1];
+                detector.findFaces(bitmap, faces);
+                if (faces[0] != null) {
+                    System.out.println(i + ": not null!");
+                    PointF p = new PointF();
+                    faces[0].getMidPoint(p);
+                    System.out.println(p.x + " " + p.y);
+                    System.out.println("x: " + faces[0].pose(FaceDetector.Face.EULER_X));
+                    System.out.println("y: " + faces[0].pose(FaceDetector.Face.EULER_Y));
+                    System.out.println("z: " + faces[0].pose(FaceDetector.Face.EULER_Z));
+//                    Settings.System.putInt(getContentResolver(),Settings.System. USER_ROTATION, i);
+//                    break;
+                }
+            }
+
+        }
+    };
+
+    /**
+     * 对焦
+     */
+    public void focus(View view) {
+        // 对焦都是硬件设备来完成的
+        camera.autoFocus(null);
+    }
+
+    /**
+     * 当Activity被销毁的时候，一定要移除监听
+     */
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, ">>>> onDestroy()");
+        if (null != callback) surfaceHolder.removeCallback(callback);
+    }
+
+
+    private Bitmap rotateBitmap(Bitmap origin, float alpha) {
+        if (origin == null) {
+            return null;
+        }
+        int width = origin.getWidth();
+        int height = origin.getHeight();
+        Matrix matrix = new Matrix();
+        matrix.setRotate(alpha);
+        // 围绕原地进行旋转
+        Bitmap newBM = Bitmap.createBitmap(origin, 0, 0, width, height, matrix, false);
+        if (newBM.equals(origin)) {
+            return newBM;
+        }
+//        origin.recycle();
+        return newBM;
+    }
+
+    /**
+     * 启动脸部检测，如果getMaxNumDetectedFaces()!=0说明不支持脸部检测
+     */
+    public void startFaceDetection() {
+        // Try starting Face Detection
+        Camera.Parameters params = camera.getParameters();
+        // start face detection only *after* preview has started
+        if (params.getMaxNumDetectedFaces() > 0) {
+            // mCamera supports face detection, so can start it:
+            camera.startFaceDetection();
+        } else {
+            Log.e("tag", "【FaceDetectorActivity】类的方法：【startFaceDetection】: " + "不支持");
+        }
+    }
+
+    /**
+     * 脸部检测接口
+     */
+    private class FaceDetectorListener implements Camera.FaceDetectionListener {
+        @Override
+        public void onFaceDetection(Camera.Face[] faces, Camera camera) {
+            if (faces.length > 0) {
+                Camera.Face face = faces[0];
+                Rect rect = face.rect;
+                Log.d("FaceDetection", "可信度：" + face.score + "face detected: " + faces.length +
+                        " Face 1 Location X: " + rect.centerX() +
+                        "Y: " + rect.centerY() + "   " + rect.left + " " + rect.top + " " + rect.right + " " + rect.bottom);
+                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: ");
+            } else {
+                // 只会执行一次
+                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: " + "没有脸部");
+            }
+        }
     }
 }
