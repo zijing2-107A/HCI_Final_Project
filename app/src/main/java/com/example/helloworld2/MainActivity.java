e043cd00a6cfa9aaddb036f522611a37909ec48c
xdbwk <xindubawukong@outlook.com>
2019-12-16 20:34:12 +0800

    seems good

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -1,30 +1,14 @@
 package com.example.helloworld2;
+
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.PointF;
 import android.graphics.Rect;
 import android.hardware.Camera;
-import android.media.FaceDetector;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -47,23 +31,15 @@ public class MainActivity extends Activity {
         // 不能直接操作SurfaceView，需要通过SurfaceView拿到SurfaceHolder
         surfaceHolder = surfaceView.getHolder();
 
-//        // 使用SurfaceHolder设置屏幕高亮，注意：所有的View都可以设置 设置屏幕高亮
-//        surfaceHolder.setKeepScreenOn(true);
+        // 使用SurfaceHolder设置屏幕高亮，注意：所有的View都可以设置 设置屏幕高亮
+        surfaceHolder.setKeepScreenOn(true);
 //
 //        // 使用SurfaceHolder设置把画面或缓存 直接显示出来
 //        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-        /**
-         * 由于要观察SurfaceView生命周期，所以需要设置监听
-         * 此监听不一样，是addCallback
-         *
-         */
         surfaceHolder.addCallback(callback);
     }
 
-    /**
-     * 定义相机对象
-     */
     private Camera camera;
 
     /**
@@ -71,16 +47,8 @@ public class MainActivity extends Activity {
      */
     private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
 
-        /**
-         * SurfaceView被创建了
-         * @param holder
-         */
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
-            /**
-             * 只有在SurfaceView生命周期方法-->SurfaceView被创建后在打开相机
-             * 以前我在 onResume 之前去打开相机，结果报错了，所以只有在这里打开相机，才是安全🔐
-             */
             // Camera要选择hardware.Camera，因为Camera属于硬件hardware
             // Camera.open(1); // 这了传入的值，可以指定为：前置摄像头/后置摄像头
             camera = Camera.open(1);
@@ -94,22 +62,11 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
                 Log.e(TAG, "相机设置预览失败");
             }
-
             camera.setFaceDetectionListener(new FaceDetectorListener());
-
-            /**
-             * 开始显示
-             */
             camera.startPreview();
         }
 
-        /**
-         * SurfaceView发生改变了
-         * @param holder
-         * @param format
-         * @param width
-         * @param height
-         */
+
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             // 以后做具体业务功能的时候，才会用到改变的值来处理
@@ -141,15 +98,8 @@ public class MainActivity extends Activity {
             }
         }
 
-        /**
-         * SurfaceView被销毁了
-         * @param holder
-         */
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
-            /**
-             * SurfaceView被销毁后，一定要释放硬件资源，Camera是硬件
-             */
             camera.stopPreview(); // (一定要有，不然只release也可能出问题)
             camera.release();
             camera = null;
@@ -157,78 +107,6 @@ public class MainActivity extends Activity {
         }
     };
 
-    /**
-     * 拍照
-     */
-    public void takepicture(View view) {
-        if (null != camera) {
-            /**
-             * 参数一：相机的快门，咔嚓，这里用不到
-             * 参数二：相机原始的数据
-             * 参数三：相机处理后的数据
-             */
-            camera.takePicture(null, null, pictureCallback);
-        }
-    }
-
-    /**
-     * 相机处理后的数据回调
-     */
-    private Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
-        /**
-         * 拍照完成，可以拿到数据了
-         * @param data 数据
-         * @param camera
-         */
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-//            // 写在指定目录 sdcard 中去
-//            File file = new File("/storage/Pictures/Screenshots", "pictureCallback.jpg");
-//            try {
-//                // 字节文件输出流，把byte[]数据写入到文件里面去
-//                OutputStream os = new FileOutputStream(file);
-//                os.write(data);
-//
-//                // 关闭字节文件输出流
-//                os.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                Log.e(TAG, "保存失败");
-//                Toast.makeText(MainActivity.this, "照片保存失败", Toast.LENGTH_SHORT).show();
-//            }
-            Bitmap bitmap0 = BitmapFactory.decodeByteArray(data,0,data.length);
-            bitmap0 = bitmap0.copy(Bitmap.Config.RGB_565, true);
-            System.out.println("onPictureTaken");
-            System.out.println("width: " + bitmap0.getWidth());
-            System.out.println("height: " + bitmap0.getHeight());
-            for (int i = 0; i < 4; i++) {
-                Bitmap bitmap = rotateBitmap(bitmap0, 90 * i);
-                FaceDetector detector = new FaceDetector(bitmap.getWidth(),bitmap.getHeight(), 1);
-                FaceDetector.Face[] faces = new FaceDetector.Face[1];
-                detector.findFaces(bitmap, faces);
-                if (faces[0] != null) {
-                    System.out.println(i + ": not null!");
-                    PointF p = new PointF();
-                    faces[0].getMidPoint(p);
-                    System.out.println(p.x + " " + p.y);
-                    System.out.println("x: " + faces[0].pose(FaceDetector.Face.EULER_X));
-                    System.out.println("y: " + faces[0].pose(FaceDetector.Face.EULER_Y));
-                    System.out.println("z: " + faces[0].pose(FaceDetector.Face.EULER_Z));
-//                    Settings.System.putInt(getContentResolver(),Settings.System. USER_ROTATION, i);
-//                    break;
-                }
-            }
-
-        }
-    };
-
-    /**
-     * 对焦
-     */
-    public void focus(View view) {
-        // 对焦都是硬件设备来完成的
-        camera.autoFocus(null);
-    }
 
     /**
      * 当Activity被销毁的时候，一定要移除监听
@@ -240,23 +118,6 @@ public class MainActivity extends Activity {
         if (null != callback) surfaceHolder.removeCallback(callback);
     }
 
-
-    private Bitmap rotateBitmap(Bitmap origin, float alpha) {
-        if (origin == null) {
-            return null;
-        }
-        int width = origin.getWidth();
-        int height = origin.getHeight();
-        Matrix matrix = new Matrix();
-        matrix.setRotate(alpha);
-        // 围绕原地进行旋转
-        Bitmap newBM = Bitmap.createBitmap(origin, 0, 0, width, height, matrix, false);
-        if (newBM.equals(origin)) {
-            return newBM;
-        }
-//        origin.recycle();
-        return newBM;
-    }
 
     /**
      * 启动脸部检测，如果getMaxNumDetectedFaces()!=0说明不支持脸部检测
@@ -282,13 +143,13 @@ public class MainActivity extends Activity {
             if (faces.length > 0) {
                 Camera.Face face = faces[0];
                 Rect rect = face.rect;
-                Log.d("FaceDetection", "可信度：" + face.score + "face detected: " + faces.length +
-                        " Face 1 Location X: " + rect.centerX() +
-                        "Y: " + rect.centerY() + "   " + rect.left + " " + rect.top + " " + rect.right + " " + rect.bottom);
-                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: ");
+                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: 检测到人脸");
+                Log.d("tag", "Left eye: (" + face.leftEye.x + ", " + face.leftEye.y + ")");
+                Log.d("tag", "Right eye: (" + face.rightEye.x + ", " + face.rightEye.y + ")");
+                Log.d("tag", "Mouth: (" + face.mouth.x + ", " + face.mouth.y + ")");
             } else {
                 // 只会执行一次
-                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: " + "没有脸部");
+                Log.e("tag", "【FaceDetectorListener】类的方法：【onFaceDetection】: 没有人脸");
             }
         }
     }
